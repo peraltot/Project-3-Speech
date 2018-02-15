1b5b8a6c9bd6611c189b0b367ad6e249b1037c72
Louise Hayes <louise@deewhy.ie>
2018-02-15 11:27:59 -0500

    new folders files for our components

--- routes/api.js
+++ routes/api.js
@@ -31,6 +31,17 @@ api.get('/', (req, res) => {
   });
 });
 
+//sample: to test search to external api if we need this later
+
+const BASEURL = "https://www.omdbapi.com/?t=";
+const APIKEY = "&apikey=trilogy";
+
+export default {
+  search: function(query) {
+    return axios.get(BASEURL + query + APIKEY);
+  }
+};
+
 
 
 api.get("/stories", function(req, res) {
@@ -49,6 +60,24 @@ api.get("/stories", function(req, res) {
       });
   });
   
+//find one story - to do - add in .populate (user)
+  api.get("/stories/:id", function(req, res) {
+    // Find all Notes
+    db.Story
+      .findOne({
+        _id: req.params.id
+      })
+      .then(function(dbStory) {
+        // If all Notes are successfully found, send them back to the client
+        res.json(dbStory);
+        // res.render('stories', dbStory);
+      })
+      .catch(function(err) {
+        // If an error occurs, send the error back to the client
+        res.json(err);
+      });
+  });
+
   // Route for saving a new story
   api.post("/saved", function(req, res) {
     db.Story
