82a6ec8f18c6244ddccc7691ce427277e3f633b7
peraltot <peraltot@gmail.com>
2018-02-15 23:25:23 -0500

    added stories page route

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ import Navbar from "./src/components/Navbar";
 
 import Home from "./src/components/pages/Home";
 import About from "./src/components/pages/About";
+import StoryDetail from "./src/components/StoryDetail";
 // import Blog from "./components/pages/Blog";
 // import Contact from "./components/pages/Contact";
 
@@ -17,6 +18,7 @@ const App = () =>
       <Navbar />
       <Route exact path="/" component={Home} />
       <Route exact path="/about" component={About} />
+      <Route exact path="/StoryDetail" component={StoryDetail} />
       {/* <Route exact path="/about" component={About} />
       <Route exact path="/blog" component={Blog} />
       <Route path="/contact" component={Contact} /> */}
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,13 +6,17 @@ const Navbar = () =>
     <div className="nav-wrapper">
       <a href="#!" className="brand-logo">Logo</a>
       <a href="#" data-activates="mobile-demo" className="button-collapse"><i className="material-icons">menu</i></a>
-      <ul className="right hide-on-med-and-down">
+      <ul className="left hide-on-med-and-down">
         <li className={window.location.pathname === "/" ? "active" : ""}>Home</li>
+        {/* </ul> */}
         <Link to="/">Home</Link>
         {/* Link is componenet router-dom gives you */}
         {/* when you click it shos it active */}
+        {/* <ul className="right hide-on-med-and-down"> */}
         <li className={window.location.pathname === "/about" ? "active" : ""}>About</li>
         <Link to="/about">About</Link>
+        <li className={window.location.pathname === "/StoryDetail" ? "active" : ""}>Stories</li>
+        <Link to="/StoryDetail">Stories</Link>
       </ul>
       {/* <ul className="side-nav" id="mobile-demo">
         <li className={window.location.pathname === "/" ? "active" : ""}>Home</li>
