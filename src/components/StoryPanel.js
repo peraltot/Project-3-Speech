deca5ec171dca36bab3f2fee44c1e0cb219237e2
Louise Hayes <louise@deewhy.ie>
2018-02-17 18:11:29 -0500

    view stories working

--- src/components/StoryPanel.js
+++ src/components/StoryPanel.js
@@ -1,13 +1,10 @@
 import React from "react";
 import {Row, Col, CardPanel} from "react-materialize";
+// import React, { Component } from "react";
+// import API from "../utils/api-axios";
+import { Link } from "react-router-dom";
+import { List, ListItem } from "../components/List";
 
-const StoryPanel = props =>
-    <Row>
-        <Col s={20} m={10}>
-            <CardPanel className="blue-grey darken-1">
-                <span>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively. I am similar to what is called a panel in other frameworks.</span>
-            </CardPanel>
-        </Col>
-    </Row>;
+
 
 export default StoryPanel;
--- src/components/pages/StoryDetail.js
+++ src/components/pages/StoryDetail.js
@@ -1,10 +1,57 @@
-import React from "react";
-import StoryPanel from "../../components/StoryPanel";
+import React, { Component } from "react";
+// import StoryPanel from '../../components/StoryPanel';
+import {Row, Col, CardPanel} from "react-materialize";
+import API from "../../utils/api-axios";
 
-const StoryDetail = () =>
-<div>
-<StoryPanel/>
-</div>;
 
+// const list = [
+//   {title:"one"},
+//   {title:"two"}
+// ]
+class StoryDetail extends Component {
+
+  constructor(props) {
+
+
+    super(props)
+    this.state = {stories:[], title:"", words:""}
+    this.loadStories = this.loadStories.bind(this);
+  }
+
+  // componentDidMount() {
+  //   this.loadStories();
+  // };
+
+  
+  
+   loadStories (){
+    console.log('blah');
+    API.getStories()
+      .then(res =>
+        this.setState({ stories: res.data })
+      )
+      .catch(err => console.log(err));
+  };
+render() {
+  return (
+   <Row>
+      <Col s={20} m={10}>
+          <CardPanel className="blue-grey darken-1">
+              <h1>View All Stories</h1>
+              <button
+                onClick={this.loadStories}
+                type="button"
+              >
+                View Stories
+              </button>
+            {this.state.stories.map(function(story) {
+            return <div>{story.title} and {story.words}</div>
+            })}
+          </CardPanel>
+      </Col>
+      </Row>
+);
+}
+}  
 
 export default StoryDetail;
