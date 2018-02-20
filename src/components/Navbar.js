86f5eb679675311e78112ce5bb74a83b1591efd4
Louise Hayes <louise@deewhy.ie>
2018-02-20 04:07:25 -0500

    changed to All Stories in navbar,js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -27,26 +27,13 @@ const Navbar = props =>
     <ul id="dropdown1"  className="dropdown-content">
       {/* <li className={window.location.pathname === "/" ? "active" : ""}></li> */}
       <Link to="/">Home</Link>
-<<<<<<< HEAD
       {/* <li className={window.location.pathname === "/about" ? "active" : ""}></li> */}
       <Link to="/about">About</Link>
       {/* <li className={window.location.pathname === "/" ? "active" : ""}></li> */}
       {/* <li className={window.location.pathname === "/StoryDetail" ? "active" : ""}></li> */}
-      <Link to="/StoryDetail">Stories</Link>
+      <Link to="/AllStories">Stories</Link>
       {/* <li className={window.location.pathname === "/" ? "active" : ""}></li> */}
     
-=======
-      <li className={window.location.pathname === "/about" ? "active" : ""}></li>
-      <Link to="/about">About</Link><li className={window.location.pathname === "/" ? "active" : ""}></li>
-      <li className={window.location.pathname === "/AllStories" ? "active" : ""}></li>
-      <Link to="/AllStories">Stories</Link><li className={window.location.pathname === "/" ? "active" : ""}></li>
-
-      {/* <li><a href="/">Home</a></li>
-      <li><a href="/about">About</a></li>
-      <li><a href="/StoryDetial">StoreyDetail</a></li> */}
-
-
->>>>>>> e7b47f8983cf056960f5b7b735917132c3e644da
     </ul>
   </div>
 
