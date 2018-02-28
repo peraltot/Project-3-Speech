6e03887ecec84e3152f6e5b75942861535541f8e
peraltot <peraltot@gmail.com>
2018-02-27 21:24:56 -0500

    Stories_layout

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ import { Link } from "react-router-dom";
 
 //   },
 //   render() {
-//     return (
+// return (
 //       <nav>
 //         <div className="nav-wrapper">
 //           <a href="/" className="brand-logo">ChatterDocs</a>
@@ -23,36 +23,76 @@ import { Link } from "react-router-dom";
 // });
 
 const Navbar = props =>
+	//  {/* <nav  className="teal lighten-2"> */ }
+	// {/* <div className="navigation">
+	// <ul id="dropdown1"  className="dropdown-content font-size=36px">
+	//   <Link to="/">Home</Link>
+	//   <Link to="/About">About</Link>
+	//   <Link to="/AllStories">Stories</Link>
+	// </ul>
+	// {/* //   </div> */} */}
 
-<nav  className="teal lighten-2">
-    {/* <div className="navigation">
-    <ul id="dropdown1"  className="dropdown-content font-size=36px">
-      <Link to="/">Home</Link>
-      <Link to="/About">About</Link>
-      <Link to="/AllStories">Stories</Link>
-    </ul>
-  </div> */}
+	// {<div className="nav-wrapper teal lighten-2" id="navigation-bar">  
+	// <a href="/" className="brand-logo right">
+	// <p className="site-title">ChatterDox</p>
+	// </a>
+	// {/* <ul className="right"> */ }
+	// {/* <ul id="nav-mobile" class="left hide-on-med-and-down"> */ }
 
-    <div className="nav-wrapper teal lighten-2" id="navigation-bar">  
-			<a href="/" className="brand-logo right">
-				<p className="site-title">ChatterDox</p>
-			</a>
-			{/* <ul className="right"> */}
-			<ul id="nav-mobile" class="left hide-on-med-and-down">
+	// {/* <li> */ }
+	// <a>
+	// {/* <a className="dropdown-button" href="#!" data-activates="dropdown1"> */ }
+	// {/* <i className="material-icons left" id="nav-icon"><Link to="/">Home</Link> */ }
+	// {/* </i> */ }
+	// {/* <i className="material-icons left" id="nav-icon"> */ }
+	// {/* <Link to="/About">About</Link> */ }
+	// {/* </i> */ }
+	// {/* <i className="material-icons left" id="nav-icon">  */ }
+	// {/* <Link to="/AllStories">Stories</Link></i> */ }
+	// </a >
+	// {/* </li> */ }
+	// {/* </ul> */ }
+	// {/* </div> */ }
+	// {/* // </nav> */ } 
+	// <div className="main-container">
+		< nav className="navbar navbar-default" role="navigation" >
+			{/* <div className="container-fluid"> */}
+				{/* <div className="navbar-header"> */}
+					<button className="base--button_clear">
+					<span className="nav navbar-nav navbar-left"></span>
+						{/* <span className="sr-only">Toggle navigation</span> */}
+						{/* <span className="icon-bar"><a className="nav navbar-nav navbar-left"><Link to="/">Home</Link></a></span> */}
+						{/* <span className="icon-bar"><Link to="/">Home</Link></span> */}
+						<Link to="/">Home</Link>
+					</button>
+					<button className="base--button_clear">
+						<span className="nav navbar-nav navbar-left"></span><Link to="/About">About</Link>
+					</button>
+					<button className="base--button_clear">
+						<span className="nav navbar-nav navbar-left"></span><Link to="/AllStories">Stories</Link>
+					</button>
+					<span className="icon-bar"></span>
+			{/* </button> */}
+				{/* <ul className="nav navbar-nav navbar-left"> */}
+				{/* <a className="nav navbar-nav navbar-left"><Link to="/">Home__  </Link></a> */}
+				{/* <a className="nav navbar-nav navbar-left"><Link to="/About">About__  </Link></a> */}
+				{/* <a className="nav navbar-nav navbar-left"><Link to="/AllStories">Stories</Link></a> */}
+				<a className="nav navbar-nav navbar-right" href="#">ChatterDoX</a>
+				{/* </ul> */}
+			{/* </div> */}
 
-				{/* <li> */}
-				<a>
-					{/* <a className="dropdown-button" href="#!" data-activates="dropdown1"> */}
-					<i className="material-icons left" id="nav-icon"><Link to="/">Home</Link>
-					</i>
-					<i className="material-icons left" id="nav-icon">
-					<Link to="/About">About</Link>
-					 </i>
-					<i className="material-icons left" id="nav-icon"> 
-					<Link to="/AllStories">Stories</Link></i>
-					</a>
-				{/* </li> */}
+			{/* <div className="collapse navbar-collapse navbar-ex1-collapse">
+			<ul className="nav navbar-nav navbar-left">
+				<li><Link to="/">Home</Link></li>
+				<li><Link to="/About">About</Link></li>
+				<li><Link to="/AllStories">Stories</Link></li>
 			</ul>
-		</div>
-</nav>
+		</div> */}
+	{/* </div> */}
+</nav >
+// </div >
+//    )
+//  }
+//    });
+
 export default Navbar;
--- src/components/pages/AllStories.js
+++ src/components/pages/AllStories.js
@@ -28,11 +28,11 @@ class AllStories extends Component {
     loadStories() {
         API.getStories()
             .then(res =>
-              // to do add in error checking here if no stories display heading and no stories
-              // if (!res.data) {
-              //   console.log("no stories");
+                // to do add in error checking here if no stories display heading and no stories
+                // if (!res.data) {
+                //   console.log("no stories");
 
-              // }
+                // }
                 this.setState({ stories: res.data })
             )
             .catch(err => console.log(err));
@@ -49,35 +49,37 @@ class AllStories extends Component {
     gdUploadStory(id, words) {
         console.log("Google Drive Upload clicked");
         googleApi.init()
-        .then(() => {
-          googleApi.saveFile(id, words)
-          .then(() => {
-            alert('File uploaded');
-            console.log('File uploaded');
-          });
-        })
-        .catch(err => {
-          alert(err);
-          console.log('error uploading to google drive ' + err);
-        });
+            .then(() => {
+                googleApi.saveFile(id, words)
+                    .then(() => {
+                        alert('File uploaded');
+                        console.log('File uploaded');
+                    });
+            })
+            .catch(err => {
+                alert(err);
+                console.log('error uploading to google drive ' + err);
+            });
     };
 
     render() {
-        const allStoryPanels = this.state.stories.map(story => {
-            return (
-          
-                    <StoryPanel
-                        key={story._id}
-                        title={story.title}
-                        words={story.words}
+        // const allStoryPanels = this.state.stories.map(story => {
+        return (
 
-                    />
-            );
-        });
+            <StoryPanel
+                key={story._id}
+                title={story.title}
+                words={story.words}
+
+            />
+        );
+        // });
 
         return (
             <div>
-                {allStoryPanels}
+                {/* {allStoryPanels} */}
+                My Stories:
+                {StoryPanel}
             </div>
         )
     }
@@ -85,21 +87,29 @@ class AllStories extends Component {
     render() {
         const theButtons = this.state.stories.map(storyBtns => {
             return (
+                
                 <div key={storyBtns._id}>
-                    {storyBtns.title} and {storyBtns.words}
-                  <button onClick={() => this.delStory(storyBtns._id)} 
-                  type = "button"
-                  >
-                  Delete
+                    <h4>
+                        {storyBtns.title}:
+                    </h4>
+                    <button onClick={() => this.delStory(storyBtns._id)}
+                        type="button"
+                    >
+                        Delete
                   </button>
-                  
-                  <button onClick={() => 
-                    this.gdUploadStory(storyBtns._id, storyBtns.words)} 
-                  type = "button"
-                  >
-                  Google Drive Upload
+
+                    <button onClick={() =>
+                        this.gdUploadStory(storyBtns._id, storyBtns.words)}
+                        type="button"
+                    >
+                        Google Drive Upload
                   </button>
-                  
+                  <p>
+                   {storyBtns.words}
+                   </p>
+
+
+
                 </div>
             )
         });
