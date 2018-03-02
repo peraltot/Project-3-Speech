a0bf92edd6e906bc9e88c6b7a34732bb6a2b1f8e
Mike Sherman <msherman83@gmail.com>
2018-03-02 13:40:19 -0500

    /login route working for me but not Emma

--- app.js
+++ app.js
@@ -8,23 +8,23 @@ import Home from "./src/components/pages/Home";
 import About from "./src/components/pages/About";
 import AllStories from "./src/components/pages/AllStories";
 import Login from './src/Home/Login';
-import history from './src/history';
 
-const auth = new Auth();
 
-const handleAuthentication = ({ location }) => {
-  if (/access_token|id_token|error/.test(location.hash)) {
-    auth.handleAuthentication();
-  }
-}
+// const auth = new Auth();
+
+// const handleAuthentication = ({ location }) => {
+//   if (/access_token|id_token|error/.test(location.hash)) {
+//     auth.handleAuthentication();
+//   }
+// }
 
 
 
 const App = () =>
-  <Router history={history}>
+  <Router>
     <div>
       <Navbar />
-      <Route path="/" render={(props) => <App auth={auth} {...props} />} />
+      <Route path="/" component={Login} />
       <Route exact path="/watson" component={Home} />
       <Route exact path="/about" component={About} />
       <Route exact path="/allstories" component={AllStories} />
@@ -33,3 +33,4 @@ const App = () =>
 
 export default App;
 
+// render={(props) => <App auth={auth} {...props} />}
--- src/Home/Login.js
+++ src/Home/Login.js
@@ -1,37 +1,58 @@
 import React, { Component } from 'react';
 
+
+
 class Login extends Component {
-  login() {
-    this.props.auth.login();
-  }
+
+
+  loginModal() {
+    res.redirect("/login")
+      .catch(err => console.log(err));
+  };
+
+
   render() {
-    const { isAuthenticated } = this.props.auth;
     return (
-      <div className="container">
-        {
-          isAuthenticated() && (
-              <h4>
-                You are logged in!
-              </h4>
-            )
-        }
-        {
-          !isAuthenticated() && (
-              <h4>
-                You are not logged in! Please{' '}
-                <a
-                  style={{ cursor: 'pointer' }}
-                  onClick={this.login.bind(this)}
-                >
-                  Log In
-                </a>
-                {' '}to continue.
-              </h4>
-            )
-        }
-      </div>
-    );
+      <div>
+        <button onClick={() => this.loginModal()} >Login to ChatterDox</button>
+    </div >
+    )
   }
 }
+
+
+
+  // login() {
+  //   this.props.auth.login();
+  // }
+  // render() {
+  //   const { isAuthenticated } = this.props.auth;
+  //   return (
+  //     <div className="container">
+  //       {
+  //         isAuthenticated() && (
+  //             <h4>
+  //               You are logged in!
+  //             </h4>
+  //           )
+  //       }
+  //       {
+  //         !isAuthenticated() && (
+  //             <h4>
+  //               You are not logged in! Please{' '}
+  //               <a
+  //                 style={{ cursor: 'pointer' }}
+  //                 onClick={this.login.bind(this)}
+  //               >
+  //                 Log In
+  //               </a>
+  //               {' '}to continue.
+  //             </h4>
+  //           )
+  //       }
+  //     </div>
+  //   );
+  // }
+
 
 export default Login;
