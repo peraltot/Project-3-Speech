ccf37cb8afb56bc742a3483e834a4a10075059fb
Mike Sherman <msherman83@gmail.com>
2018-03-01 19:25:52 -0500

    failed to lookup view error

--- app.js
+++ app.js
@@ -8,8 +8,6 @@ import Home from "./src/components/pages/Home";
 import About from "./src/components/pages/About";
 import AllStories from "./src/components/pages/AllStories";
 import Login from './src/Home/Login';
-import Callback from './src/Callback/Callback';
-import Auth from './src/Auth/Auth';
 import history from './src/history';
 
 const auth = new Auth();
@@ -30,10 +28,6 @@ const App = () =>
       <Route exact path="/watson" component={Home} />
       <Route exact path="/about" component={About} />
       <Route exact path="/allstories" component={AllStories} />
-      <Route path="/callback" render={(props) => {
-        handleAuthentication(props);
-        return <Callback {...props} />
-      }} />
     </div>
   </Router>;
 
--- src/Auth/auth0-variables.js
+++
@@ -1,3 +0,0 @@
-var AUTH0_CLIENT_ID='lBSBehjiwfS3GUcbk2cWrMXJzy1v5Xzd';
-var AUTH0_DOMAIN='msherman83.auth0.com';
-var AUTH0_CALLBACK_URL=location.href;
