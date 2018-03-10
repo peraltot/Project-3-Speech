2f5d29f206084e052fdf8ccdd3e31b9348923019
Mike Sherman <msherman83@gmail.com>
2018-03-10 13:23:31 -0500

    accepted storypanel change

--- src/components/StoryPanel/StoryPanel.js
+++ src/components/StoryPanel/StoryPanel.js
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-
-const StoryPanel = (props) =>
-<div className="center-align">
-    <div className="card" id="storyCards">
-        <div className="card-content center-align black-text">
-            {props.children}
-        </div>
-    </div>
-</div>
-
-export default StoryPanel;
-=======
 // import React from "react";
 // import getMuiTheme from 'material-ui/styles/getMuiTheme';
 // import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
@@ -26,4 +12,3 @@ export default StoryPanel;
 // </div>
 
 // export default StoryPanel;
->>>>>>> c88f40b351df31572f20b8cc7d2b1fe775da88a4
