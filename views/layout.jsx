dc0d65b9f3767baa859fa87fd45feadd4cb3c88a
Louise Hayes <louise@deewhy.ie>
2018-03-14 21:31:35 -0400

    locally add materialize.css and remove modal styling to force use of watson modal css

--- views/layout.jsx
+++ views/layout.jsx
@@ -25,7 +25,7 @@ export default function Layout(props) {
         {/* Import Icons */}
         <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
         {/* Import materialize.css */}
-	      {/* <link href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/css/materialize.min.css" rel="stylesheet"/> */}
+	      <link href="/css/materialize.css" rel="stylesheet"/>
       </head>
       <body>
 
